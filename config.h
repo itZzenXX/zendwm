21dbecdb62916dc00da5dabef41097ad1bc421e9
itZzenXX <github.letting@aleeas.com>
2021-04-02 12:07:05 -0400

    main

--- config.h
+++ config.h
@@ -62,7 +62,7 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray3, "-sf", col_gray1, NULL };
 static const char *termcmd[]  = { "urxvt", NULL };
-static const char *librewolf[]  = { "librewolf-bin", NULL };
+static const char *librewolf[]  = { "librewolf", NULL };
 static const char *torbrowser[] = { "torbrowser-launcher", NULL };
 static const char *keepassxc[] = { "keepassxc", NULL };
 
